91880c8ac3301621ca8a28331f2b40b5788d0591
A24Ole <a24oleproyat@inspedralbes.cat>
2025-10-28 09:57:39 +0100

    Versión de prueba de docker

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => res.send('¡Hola, mundo desde Docker!'));
+
+// Serve static files from the Vue app
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api', (req, res) => res.send('¡Hola, mundo desde Docker!'));
+
+// Handles any requests that don't match the ones above
+app.get('/*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
 app.listen(port, () => console.log(`Servidor escuchando en http://localhost:${port}`));
