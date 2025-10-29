23b592ba7bab8f03790074ab1e0f3393e9e2006b
AlbaS-R <albasanchezromero345@gmail.com>
2025-10-29 11:49:11 +0100

    mini cambios

--- frontend/src/componentes/interfazJuego.vue
+++ frontend/src/componentes/interfazJuego.vue
@@ -1,53 +1,32 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from "vue";
+import { io } from 'socket.io-client';
 
-//Funciones y variables de el teclado
-const filesDelTeclat = ref([
-  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
-  ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-  ["Z", "X", "C", "V", "B", "N", "M"],
-]);
-const teclaDada = ref("");
 
-function presionarTecla(event) {
-  teclaDada.value = event.key.toUpperCase();
-  setTimeout(() => {
-    teclaDada.value = "";
-  }, 100);
-}
+//Funciones 
+
+
 onMounted(() => {
-  window.addEventListener("keydown", presionarTecla);
+  socket.on('game_started', (data) => {
+    
+  });
 });
-onUnmounted(() => {
-  window.removeEventListener("keydown", presionarTecla);
-});
-
-
 
 </script>
 <template>
   <div id="contenedor-juego">
     <li></li>
+
+    <div id="contenedor-mesa">
+      <span>hola
+      </span>
+    </div>
     <div class="contenedor-texto">
       <input
         type="text"
         class="text-input"
         placeholder="Comença a escriure..."
       />
-    </div>
-    <div
-      v-for="(fila, indexFila) in filesDelTeclat"
-      :key="indexFila"
-      class="fila-teclado"
-    >
-      <button
-        v-for="(letra, indexLetra) in fila"
-        :key="indexLetra"
-        class="teclado"
-        :class="{ 'tecla-premuda': letra === teclaDada }"
-      >
-        {{ letra }}
-      </button>
     </div>
   </div>
 </template>
@@ -57,12 +36,19 @@ onUnmounted(() => {
   flex-direction: column;
   align-items: center;
   text-align: center;
-  margin: 5% auto;
-  min-width: 900px;
-  min-height: 500px;
-  border-radius: 10px;
+  width: 100vw;
+  height: 100vh;
   padding: 20px;
   box-sizing: border-box;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+#contenedor-mesa{
+  background-image: url(../../public/assets/img/Mesa-sinfondo-nopixel.png);
+  background-size: cover;
+  display: flex;
+  min-width: 500px;
+  min-height: 300px;
 }
 
 .contenedor-texto {
@@ -80,30 +66,4 @@ onUnmounted(() => {
   color: wheat;
 }
 
-.teclado.tecla-premuda {
-  color: rgb(37, 37, 37);
-  background-color: rgb(248, 252, 43);
-  border-color: rgb(248, 252, 43);
-}
-
-.fila-teclado {
-  display: flex;
-  column-gap: 10px;
-  margin-top: 10px;
-  width: 100%;
-  justify-content: center;
-  flex-wrap: wrap;
-}
-
-.teclado {
-  margin-top: 10px;
-  min-width: 44px;
-  min-height: 44px;
-  border-radius: 5px;
-  font-family: Arial, Helvetica, sans-serif;
-  color: white;
-  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-  background-color: rgba(0, 0, 0, 0);
-  border-color: yellow;
-}
 </style>
