5fa3f7dd14422860699958ce4515bfeb1eca284c
Edson <a23edstorcev@inspedralbes.cat>
2025-11-11 10:39:09 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// DEPENDENCIAS
 import cors from "cors";
 import { Server } from "socket.io";
 import express from "express";
@@ -9,26 +10,27 @@ import registerRouter from "./routes/registerRoutes.js";
 
 // SOCKET.IO
 import { initializeSocketIO } from "./socket/socketInit.js";
+import { register } from "module";
 
 const app = express();
 const server = http.createServer(app);
 
-// --- Configurar CORS solo para tu frontend ---
+// CORS
 const corsOptions = {
   origin: "http://typebet.daw.inspedralbes.cat", // Solo permite este dominio
   methods: ["GET", "POST"],
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
-
-// Rutas de la API
 app.use("/palabras", wordsRouter);
-app.use("/user", registerRouter);
+app.use("/user", registerRouter);   
 
-// --- Crear instancia de Socket.IO ---
+// --- Crear **una sola instancia** de Socket.IO ---
 const io = new Server(server, { cors: corsOptions });
 
 // Registrar eventos
+// registerGameEvents(io);
 initializeSocketIO(io);
 
 io.on("connection", (socket) => {
@@ -38,9 +40,10 @@ io.on("connection", (socket) => {
   });
 });
 
+
 // --- Puerto de escucha ---
 const port = process.env.PORT || 3000;
-const host = "0.0.0.0"; // Escucha en todas las interfaces del contenedor
+const host = process.env.HOST || "localhost";
 server.listen(port, host, () => {
-  console.log(`[IO] Server listening on port ${port}`);
+  console.log(`[IO] Server listening on http://${host}:${port}`);
 });
