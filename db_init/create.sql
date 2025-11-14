55500950168dce791515560d12714f7df4753f30
a23edstorcev <a23edstorcev@inspedralbes.cat>
2025-11-14 02:27:10 +0100

    .

--- db_init/create.sql
+++ db_init/create.sql
@@ -3,24 +3,18 @@
 -- ======================================================
 SET NAMES utf8mb4;
 
-CREATE DATABASE IF NOT EXISTS Proyecto1
+CREATE DATABASE IF NOT EXISTS tr1_typebet
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES ON Proyecto1.* TO 'usuari';
-FLUSH PRIVILEGES;
-
-USE Proyecto1;
+USE tr1_typebet;
 
 -- ======================================================
 -- CREACIÓN DE TABLA
 -- ======================================================
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     username VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(500),
+    password VARCHAR(500), -- recordar hashear
     imagen VARCHAR(500)
 );
-
